d900be265b987138dc8c564d98813b3564284957
jocelyntan5 <22043453@myrp.edu.sg>
2023-05-04 18:03:06 +0800

    Completed up to Section E

--- app/src/main/java/sg/edu/rp/c346/id22043453/week03/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22043453/week03/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnDisplay;
     EditText etInput;
     ToggleButton tgButton;
+    RadioGroup rgGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         btnDisplay = findViewById(R.id.buttonDisplay);
         etInput = findViewById(R.id.editTextInput);
         tgButton = findViewById(R.id.toggleButtonEnabled);
+        rgGender = findViewById(R.id.radioGroupGender);
 
         btnDisplay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,6 +55,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code for the action
+                String stringResponse = etInput.getText().toString();
+                int checkedRadioId = rgGender.getCheckedRadioButtonId();
+                if(checkedRadioId == R.id.radioButtonGenderMale){
+                    // Write the code when male selected
+                    stringResponse = "He says " + stringResponse;
+                }
+                else{
+                    // Write the code when female selected
+                    stringResponse = "She says " + stringResponse;
+                }
+                tvDisplay.setText(stringResponse);
+            }
+        });
     }
 }
